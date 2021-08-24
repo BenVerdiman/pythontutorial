bbae77eb7860b80c781ef7a1c9a3b38c0dcdcc94
BenVerdiman <49784615+BenVerdiman@users.noreply.github.com>
2021-08-24 16:36:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+
+print("Аз съм Българче!")
+
+# -------------------------
+
+if 5 > 2:
+  print("Five is greater than two!") # IF STATEMENT
+
+# -------------------------
+
+x="Tralaalalalalalalalalalalallalalaalalalalalalalala"
+
+print(x)
+
+# -------------------------
+
+x=float("5.01")
+print(x)
+
+# -------------------------
+
+x=["Miro","Toshko","Maimuna"]
+print(x)
+#Changing the value of the 2nd element
+print(x[1])
+
+x[1] = "Toshko Afrikanski"
+print(x[1])
+
+#x= True
+#y= False
+#print(x)
+#print(y)
+# -------------------------
+y="false"
+print(y[4])
+
+#x=int("Miro")
+
+x= bool("4")
+print(x)
+
+x = bool(" ")
+print(x)
+
+x = bool("")
+print(x)
+
+
+
+
+# -------------------------
+
+x=str(2)
+print(x)
+
+x=str(True)
+print(x)
+
+# -------------------------
+
+
+import math as m
+
+# ------------------------
+
+print(((22+6)/14)**2)
+
+
+# -------------------------
+print(int(input("number:"))*2)
